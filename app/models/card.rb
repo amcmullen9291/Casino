bae4086e686f9fb135794c05fcf527d4ab383c45
amcmullen9291 <amcmullen9291@gmail.com>
2021-04-07 01:52:58 -0400

    added card and guest validations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,6 @@
 class Card < ApplicationRecord
     validates_presence_of :suit 
-    validates_presence_of :value  
-
+    validates_presence_of :value, numericality: true
+    validates_presence_of :card_number, uniqueness: true
+    validates_inclusion_of :value, :in => 1..10
 end
