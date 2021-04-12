7d09a0cd19eda9715869524c71bebedd0fbf9b27
amcmullen9291 <amcmullen9291@gmail.com>
2021-04-12 08:08:28 -0400

    added method for rand card select

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,5 +1,26 @@
 class Round < ApplicationRecord
     validates_presence_of :availableCards
     has_many :cards 
-    belongs_to :user
+    belongs_to :guest
+
+    usedCards = []   
+    @cards = Card.all.collect(&:id)
+
+    def draw
+        usedCards << @cards.shuffle!.pop
+        if usedCard[-1] > usedCards[-2]
+            return "higher"
+        else
+            return "lower"
+        end
+        return @cards.length 
+    end
+
+    def compared 
+        if usedCard[last_element-1] > usedCards[last_element]
+            return "lower"
+        else
+            return "higher"
+        end
+    end 
 end
