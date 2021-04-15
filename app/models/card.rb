8ec499083caa248a98308c4a3e29da8776fbeaf3
amcmullen9291 <amcmullen9291@gmail.com>
2021-04-15 06:59:08 -0400

    merge commit

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,7 +3,4 @@ class Card < ApplicationRecord
     validates_presence_of :value
     validates_presence_of :card_number, uniqueness: true
     has_one_attached :avatar
-    # has_attached_file :avatar, styles: {medium: "300x300", thumb: "100x100>" }
-    validates_attachment :avatar, presence: true
-    do_not_validate_attachment_file_type :avatar
 end
--- db/migrate/20210414030812_add_attachment_avatar_to_cards.rb
+++
@@ -1,9 +0,0 @@
-class AddAttachmentAvatarToCards < ActiveRecord::Migration[5.2]
-  def change
-    add_column :cards, :avatar, :attachment
-  end
-
-  def self.down
-    remove_attachment :cards, :avatar
-  end
-end
